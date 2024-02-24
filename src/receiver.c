bfe38949748265e5eb2055987b834f476b0cce0c
Tongyu Li <tonyli990322@gmail.com>
2024-02-23 16:59:09 -0800

    use UDP to build our tcp-like protocal. only have the absolute neccesary components rn.

--- src/receiver.c
+++ src/receiver.c
@@ -10,11 +10,50 @@
 
 #include <pthread.h>
 #include <errno.h>
+#include <time.h>
 
 void rrecv(unsigned short int myUDPport, 
             char* destinationFile, 
-            unsigned long long int writeRate) {
+            unsigned long long int writeRate) 
+{
+    // Create UDP socket
+    int sockfd;
+    struct sockaddr_in servaddr, cliaddr;
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd == -1) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
 
+    // Bind socket to given port
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+    servaddr.sin_port = htons(myUDPport);
+    if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) == -1) {
+        perror("Binding failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Open destination file
+    FILE *file = fopen(destinationFile, "wb");
+    if (file == NULL) {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
+    }
+
+    // Receive data and write to file
+    unsigned char buffer[MAX_PACKET_SIZE];
+    int bytesReceived;
+    while (1) {
+        socklen_t len = sizeof(cliaddr);
+        bytesReceived = recvfrom(sockfd, buffer, MAX_PACKET_SIZE, 0, (struct sockaddr *)&cliaddr, &len);
+        fwrite(buffer, 1, bytesReceived, file);
+    }
+
+    // Close file and socket
+    fclose(file);
+    close(sockfd);
 }
 
 int main(int argc, char** argv) {
@@ -31,4 +70,8 @@ int main(int argc, char** argv) {
     }
 
     udpPort = (unsigned short int) atoi(argv[1]);
+    filename = argv[2];
+    writeRate = 0;
+
+    rrecv(myUBPport, filename, writeRate);
 }
--- src/sender.c
+++ src/sender.c
@@ -10,13 +10,70 @@
 
 #include <pthread.h>
 #include <errno.h>
+#include <time.h>
+
+// Time related
+clock_t prev_time = clock();
+
+int seconds_since_last_clock() {
+    int result = difftime(clock(), prev_time);
+    prev_time = clock();
+    return result;
+}
+
+// Handshaking Protocal
+
+
+// Packet Structure
+
+
+// Congestion control
+
+
+// retransmission
+
+
+//
+
 
 void rsend(char* hostname, 
             unsigned short int hostUDPport, 
             char* filename, 
             unsigned long long int bytesToTransfer) 
 {
+    // Create UDP socket
+    int sockfd;
+    struct sockaddr_in servaddr;
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd == -1) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
 
+    // Fill server information
+    memset(&servaddr, 0, sizeof(servaddr));
+    servaddr.sin_family = AF_INET; //IPv4
+    servaddr.sin_port = htons(hostUDPport);
+    servaddr.sin_addr.s_addr = inet_addr(hostname);
+
+    // Open file to send
+    FILE *file = fopen(filename, "rb");
+    if (file == NULL) {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
+    }
+
+    // Send file data over UDP
+    unsigned char buffer[bytesToTransfer];
+    int bytesRead;
+    while ((bytesRead = fread(buffer, 1, bytesToTransfer, file)) > 0 && bytesToTransfer > 0) {
+        sendto(sockfd, buffer, bytesRead, 0, (const struct sockaddr *)&servaddr, sizeof(servaddr));
+        bytesToTransfer -= bytesRead;
+    }
+
+    // Close file and socket
+    fclose(file);
+    close(sockfd);
 }
 
 int main(int argc, char** argv) {
@@ -35,6 +92,9 @@ int main(int argc, char** argv) {
     hostUDPport = (unsigned short int) atoi(argv[2]);
     hostname = argv[1];
     bytesToTransfer = atoll(argv[4]);
+    file_name = argv[3];
+
+    rsend(hostname, hostUDPportmb, file_name, bytesToTransfer);
 
     return (EXIT_SUCCESS);
 }
